45cd19889abc1d5f85e19401c00fbf7def2e475e
Azuria <74812558+Azuria03@users.noreply.github.com>
2026-02-10 01:13:55 +0800

    Day 6 FORMS, Java intro - let,const,console,booleans,!,===,!==,NOTES UPDATE

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ console.log(place);
 let time=5;
 time=true
 console.log(!time+" hello 10")
+
+let test = 5;
+console.log(test===5);
+console.log(test!==5);
+
+const test1=test+5;
+console.log(test!==test1);
+const test3="good";
+console.log(test3);
+test = 10;
+console.log(test===test1);
